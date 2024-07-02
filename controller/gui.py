88db93d32f00c59e37f298864676e47d0bdb75a0
mizanulridhoaohana2002 <mizanulridhoaohana2002@mail.ugm.ac.id>
2024-07-02 22:28:29 +0800

    update controller

--- controller/gui.py
+++ controller/gui.py
@@ -38,34 +38,158 @@ canvas.place(x = 0, y = 0)
 image_image_1 = PhotoImage(
     file=relative_to_assets("image_1.png"))
 image_1 = canvas.create_image(
-    1099.333251953125,
-    22.33349609375,
+    382.0,
+    405.0,
     image=image_image_1
+)
+
+canvas.create_text(
+    86.0,
+    219.5,
+    anchor="nw",
+    text="Alma Liakua Mutia",
+    fill="#404040",
+    font=("Nunito Bold", 24 * -1)
+)
+
+canvas.create_text(
+    86.0,
+    278.5,
+    anchor="nw",
+    text="Jenis Kelamin               : ",
+    fill="#404040",
+    font=("Nunito Regular", 16 * -1)
+)
+
+canvas.create_text(
+    257.0,
+    278.5,
+    anchor="nw",
+    text="Perempuan",
+    fill="#404040",
+    font=("Nunito Regular", 16 * -1)
+)
+
+canvas.create_text(
+    86.0,
+    312.5,
+    anchor="nw",
+    text="Tanggal Lahir              : ",
+    fill="#404040",
+    font=("Nunito Regular", 16 * -1)
+)
+
+canvas.create_text(
+    257.0,
+    312.5,
+    anchor="nw",
+    text="Selasa, 16 April 2024",
+    fill="#404040",
+    font=("Nunito Regular", 16 * -1)
+)
+
+canvas.create_text(
+    86.0,
+    346.5,
+    anchor="nw",
+    text="Alamat Domisili           : ",
+    fill="#404040",
+    font=("Nunito Regular", 16 * -1)
+)
+
+canvas.create_text(
+    257.0,
+    346.5,
+    anchor="nw",
+    text="Jl. Langko No. 72, Mataram, Ntb",
+    fill="#404040",
+    font=("Nunito Regular", 16 * -1)
+)
+
+canvas.create_text(
+    86.0,
+    380.5,
+    anchor="nw",
+    text="Diagnosa Terakhir       :",
+    fill="#404040",
+    font=("Nunito Regular", 16 * -1)
+)
+
+canvas.create_text(
+    257.0,
+    384.5,
+    anchor="nw",
+    text="OMA PERFORASI",
+    fill="#1E5C2A",
+    font=("Nunito Bold", 19 * -1)
+)
+
+canvas.create_text(
+    86.0,
+    417.5,
+    anchor="nw",
+    text="OMA Perofrasi adalah gangguan ........ . Dicirikan dengan .......Lorem ipsum dolor sit amet, consectetur adipiscing elit. Iaculis tempus tellus adipiscing eget non arcu egestas elementum faucibus. Senectus cras nunc et, arcu ultricies tristique. Mi purus ut eget euismod orci, odio eu, non. Massa sapien magna volutpat lorem. Aliquet amet elit sed ac. ",
+    fill="#404040",
+    font=("Nunito SemiBold", 16 * -1)
 )
 
 image_image_2 = PhotoImage(
     file=relative_to_assets("image_2.png"))
 image_2 = canvas.create_image(
-    1074.0,
-    22.33056640625,
+    897.0,
+    339.0,
     image=image_image_2
 )
 
+canvas.create_rectangle(
+    738.0,
+    349.99997228697987,
+    1056.0,
+    351.0,
+    fill="#E0E0E0",
+    outline="")
+
 canvas.create_text(
-    21.0,
-    13.0,
+    744.5,
+    277.5,
     anchor="nw",
-    text="9:41",
-    fill="#FFFFFF",
-    font=("SFProText Semibold", 15 * -1)
+    text="Penyakit Telinga",
+    fill="#404040",
+    font=("Nunito Bold", 19 * -1)
 )
 
 image_image_3 = PhotoImage(
     file=relative_to_assets("image_3.png"))
 image_3 = canvas.create_image(
-    566.0,
-    89.0,
+    754.5,
+    313.0,
     image=image_image_3
+)
+
+canvas.create_text(
+    774.5,
+    301.5,
+    anchor="nw",
+    text="02 Januari 2024",
+    fill="#404040",
+    font=("Nunito Regular", 12 * -1)
+)
+
+image_image_4 = PhotoImage(
+    file=relative_to_assets("image_4.png"))
+image_4 = canvas.create_image(
+    754.5,
+    334.5,
+    image=image_image_4
+)
+
+canvas.create_text(
+    773.5,
+    326.0,
+    anchor="nw",
+    text="RS. Provinsi Nusa Tenggara Barat",
+    fill="#404040",
+    font=("Nunito Regular", 12 * -1)
 )
 
 button_image_1 = PhotoImage(
@@ -78,35 +202,61 @@ button_1 = Button(
     relief="flat"
 )
 button_1.place(
-    x=455.0,
-    y=601.0,
-    width=233.0,
-    height=78.0
+    x=994.5,
+    y=289.0,
+    width=44.0,
+    height=44.0
 )
 
-image_image_4 = PhotoImage(
-    file=relative_to_assets("image_4.png"))
-image_4 = canvas.create_image(
-    566.0,
-    365.0,
-    image=image_image_4
+canvas.create_rectangle(
+    738.0,
+    430.49997228697987,
+    1056.0,
+    431.5,
+    fill="#E0E0E0",
+    outline="")
+
+canvas.create_text(
+    744.5,
+    358.0,
+    anchor="nw",
+    text="Penyakit Telinga",
+    fill="#404040",
+    font=("Nunito Bold", 19 * -1)
 )
 
 image_image_5 = PhotoImage(
     file=relative_to_assets("image_5.png"))
 image_5 = canvas.create_image(
-    306.0,
-    280.0,
+    754.5,
+    393.5,
     image=image_image_5
 )
 
 canvas.create_text(
-    88.0,
-    249.0,
+    774.5,
+    382.0,
     anchor="nw",
-    text="Sprayer",
+    text="02 Januari 2024",
     fill="#404040",
-    font=("Nunito Bold", 34 * -1)
+    font=("Nunito Regular", 12 * -1)
+)
+
+image_image_6 = PhotoImage(
+    file=relative_to_assets("image_6.png"))
+image_6 = canvas.create_image(
+    754.5,
+    415.0,
+    image=image_image_6
+)
+
+canvas.create_text(
+    773.5,
+    406.5,
+    anchor="nw",
+    text="RS. Provinsi Nusa Tenggara Barat",
+    fill="#404040",
+    font=("Nunito Regular", 12 * -1)
 )
 
 button_image_2 = PhotoImage(
@@ -119,18 +269,116 @@ button_2 = Button(
     relief="flat"
 )
 button_2.place(
-    x=424.9998474121094,
-    y=346.7379150390625,
-    width=113.17479705810547,
-    height=113.17479705810547
+    x=994.5,
+    y=369.5,
+    width=44.0,
+    height=44.0
 )
 
-image_image_6 = PhotoImage(
-    file=relative_to_assets("image_6.png"))
-image_6 = canvas.create_image(
-    305.9998474121094,
-    413.3253173828125,
-    image=image_image_6
+canvas.create_text(
+    752.0,
+    224.0,
+    anchor="nw",
+    text="Riwayat Pemeriksaan",
+    fill="#404040",
+    font=("Nunito Bold", 24 * -1)
+)
+
+image_image_7 = PhotoImage(
+    file=relative_to_assets("image_7.png"))
+image_7 = canvas.create_image(
+    897.0,
+    592.0,
+    image=image_image_7
+)
+
+canvas.create_text(
+    752.0,
+    520.0,
+    anchor="nw",
+    text="Asuransi Kesehatan",
+    fill="#404040",
+    font=("Nunito Bold", 24 * -1)
+)
+
+canvas.create_text(
+    752.0,
+    570.0,
+    anchor="nw",
+    text="No Asuransi     : ",
+    fill="#404040",
+    font=("Nunito Regular", 15 * -1)
+)
+
+canvas.create_text(
+    865.0,
+    570.0,
+    anchor="nw",
+    text="000863002321023",
+    fill="#404040",
+    font=("Nunito Bold", 15 * -1)
+)
+
+canvas.create_text(
+    752.0,
+    594.0,
+    anchor="nw",
+    text="Jenis Asuransi  : ",
+    fill="#404040",
+    font=("Nunito Regular", 15 * -1)
+)
+
+canvas.create_text(
+    864.0,
+    594.0,
+    anchor="nw",
+    text="BPJS",
+    fill="#404040",
+    font=("Nunito Bold", 15 * -1)
+)
+
+canvas.create_text(
+    752.0,
+    618.0,
+    anchor="nw",
+    text="Kelas Asuransi : ",
+    fill="#404040",
+    font=("Nunito Regular", 15 * -1)
+)
+
+canvas.create_text(
+    864.0,
+    618.0,
+    anchor="nw",
+    text="Kelas 1",
+    fill="#404040",
+    font=("Nunito Bold", 15 * -1)
+)
+
+canvas.create_text(
+    752.0,
+    642.0,
+    anchor="nw",
+    text="Fasilitas Kesehatan : ",
+    fill="#404040",
+    font=("Nunito Regular", 15 * -1)
+)
+
+canvas.create_text(
+    891.0,
+    642.0,
+    anchor="nw",
+    text="Puskesmas Selaparang",
+    fill="#404040",
+    font=("Nunito Bold", 15 * -1)
+)
+
+image_image_8 = PhotoImage(
+    file=relative_to_assets("image_8.png"))
+image_8 = canvas.create_image(
+    566.0,
+    89.0,
+    image=image_image_8
 )
 
 button_image_3 = PhotoImage(
@@ -143,36 +391,10 @@ button_3 = Button(
     relief="flat"
 )
 button_3.place(
-    x=45.82501220703125,
-    y=346.7379150390625,
-    width=113.1748046875,
-    height=113.1748046875
-)
-
-canvas.create_text(
-    209.99984741210938,
-    381.0001220703125,
-    anchor="nw",
-    text="0",
-    fill="#404040",
-    font=("Nunito Bold", 30 * -1)
-)
-
-image_image_7 = PhotoImage(
-    file=relative_to_assets("image_7.png"))
-image_7 = canvas.create_image(
-    860.1749877929688,
-    280.0,
-    image=image_image_7
-)
-
-canvas.create_text(
-    642.1749877929688,
-    249.0,
-    anchor="nw",
-    text="Vacuum",
-    fill="#404040",
-    font=("Nunito Bold", 34 * -1)
+    x=59.5,
+    y=633.5,
+    width=192.0,
+    height=54.0
 )
 
 button_image_4 = PhotoImage(
@@ -185,18 +407,10 @@ button_4 = Button(
     relief="flat"
 )
 button_4.place(
-    x=971.1748046875,
-    y=346.7379150390625,
-    width=113.17479705810547,
-    height=113.17479705810547
-)
-
-image_image_8 = PhotoImage(
-    file=relative_to_assets("image_8.png"))
-image_8 = canvas.create_image(
-    860.1748046875,
-    413.3253173828125,
-    image=image_image_8
+    x=284.5,
+    y=633.5,
+    width=192.0,
+    height=54.0
 )
 
 button_image_5 = PhotoImage(
@@ -209,19 +423,61 @@ button_5 = Button(
     relief="flat"
 )
 button_5.place(
-    x=600.0,
-    y=346.7379150390625,
-    width=113.1748046875,
-    height=113.1748046875
+    x=509.5,
+    y=633.5,
+    width=192.0,
+    height=54.0
 )
 
 canvas.create_text(
-    764.1748046875,
-    381.0001220703125,
+    130.0,
+    117.5,
     anchor="nw",
-    text="0",
-    fill="#404040",
-    font=("Nunito Bold", 30 * -1)
+    text="RS. Universitas Mataram",
+    fill="#FFFFFF",
+    font=("Nunito Black", 14 * -1)
+)
+
+canvas.create_text(
+    130.0,
+    144.5,
+    anchor="nw",
+    text="Unit THT",
+    fill="#F1F1F1",
+    font=("Nunito SemiBold", 12 * -1)
+)
+
+image_image_9 = PhotoImage(
+    file=relative_to_assets("image_9.png"))
+image_9 = canvas.create_image(
+    89.0,
+    139.0,
+    image=image_image_9
+)
+
+image_image_10 = PhotoImage(
+    file=relative_to_assets("image_10.png"))
+image_10 = canvas.create_image(
+    1099.333251953125,
+    22.33349609375,
+    image=image_image_10
+)
+
+image_image_11 = PhotoImage(
+    file=relative_to_assets("image_11.png"))
+image_11 = canvas.create_image(
+    1074.0,
+    22.33056640625,
+    image=image_image_11
+)
+
+canvas.create_text(
+    21.0,
+    13.0,
+    anchor="nw",
+    text="9:41",
+    fill="#FFFFFF",
+    font=("SFProText Semibold", 15 * -1)
 )
 window.resizable(False, False)
 window.mainloop()
